b9138d0d4e9bff173bf761520b0b3bbfdbafe375
danny pratt <dpratt11@ymail.com>
2025-01-27 16:47:07 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+//import java.util.*;
+/*
+ * IT-2660 - Lab 1
+ * Student Name: Daniel Pratt 
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hello, world!");
+
+    Lab1 labs = new Lab1();
+    System.out.println(labs.increment(1));
+  }
+}
+
+
+// Add all of the methods here
+class Lab1 {
+  public int increment(int num) {
+    return ++num;
+  }
+  public int hello () {
+    return 0;
+  }
+}
