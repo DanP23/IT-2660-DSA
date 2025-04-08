fa4e39db04bac2e48f147dedb3be4270757a86b1
danny pratt <dpratt11@ymail.com>
2025-04-08 19:46:32 -0400

    lab 6 rough draft

--- Lab-6/Main.java
+++
@@ -1,6 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    // Use the instructions in Blackboard or instructions.md to complete Lab 6
-    
-  }
-}
---
+++ Lab6/Main.java
@@ -0,0 +1,35 @@
+/*
+ * IT-2660-DSA - Lab 6
+ * Student Name: Daniel Pratt
+ */
+package Lab6;
+
+
+class Main {
+  public static void main(String[] args) {
+    // Use the instructions in Blackboard or instructions.md to complete Lab 6
+
+    MyHashMap<String, Integer> creditHours = new MyHashMap<>();
+    
+    creditHours.put("IT-1025", 3);
+    creditHours.put("IT-1050", 3);
+    creditHours.put("IT-1150", 3);
+    creditHours.put("IT-2310", 3);
+    creditHours.put("IT-2320", 4);
+    creditHours.put("IT-2351", 4);
+    creditHours.put("IT-2650", 4);
+    creditHours.put("IT-2660", 4);
+    creditHours.put("IT-2030", 4);
+
+  
+    System.out.println("is IT-1025 in table: " + creditHours.containsKey("IT-1025"));
+    System.out.println("is IT-2110 in table: " + creditHours.containsKey("IT-2110"));
+    System.out.println(creditHours.entrySet());
+
+    creditHours.remove("IT-2030");
+    creditHours.remove("IT-1150");
+
+    System.out.println(creditHours.entrySet());
+
+  }
+}
