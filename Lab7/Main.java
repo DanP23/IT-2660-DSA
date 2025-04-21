951e4118c8ddd87e49a24845245d379bb1e0c4d2
danny pratt <dpratt11@ymail.com>
2025-04-21 17:54:47 -0400

    Lab rough draft

--- Lab7/Main.java
+++ Lab7/Main.java
@@ -11,11 +11,53 @@ class Main {
     String[] vertices = {};
 
     int[][] edges = {
-      {0,1}
+      
     };
 
+
     Graph<String> graph = new UnweightedGraph<>(vertices, edges);
+    graph.addVertex("Business & Technology");
+    graph.addVertex("Liberal Arts");
+    graph.addVertex("Student Services");
+    graph.addVertex("Health Careers & Sciences");
+    graph.addVertex("Health Technologies Center");
+    graph.addVertex("Recreation Center");
+    graph.addVertex("Technology Learning Center");
+    graph.addVertex("Theatre");
+
+    // edges represent directly linked areas
+    graph.addEdge(new Edge(0, 7));
+    graph.addEdge(new Edge(0, 2));
+    graph.addEdge(new Edge(1, 7));
+    graph.addEdge(new Edge(1, 2));
+    graph.addEdge(new Edge(2, 1));
+    graph.addEdge(new Edge(2, 3));
+    graph.addEdge(new Edge(3, 2));
+    graph.addEdge(new Edge(3, 4));
+    graph.addEdge(new Edge(4, 3));
+    graph.addEdge(new Edge(5, 2));
+    graph.addEdge(new Edge(5, 4));
+    graph.addEdge(new Edge(6, 2));
+    graph.addEdge(new Edge(7, 0));
+    graph.addEdge(new Edge(7, 1));
+
+
     UnweightedGraph<String>.SearchTree dfs = graph.dfs(graph.getIndex("Business & Technology")); // Get a dfs starting at the Business and Technology Building. Change this is you called it something different in your vertices!
+
+    // path from Business and Technology to Health Tech Center
+    dfs.printPath(4);
+    System.out.println();
+
+    // path from Business and Technology to Student Services
+    dfs.printPath(2);
+    System.out.println();
+
+    // path from Business and Technology to Recreation Center
+    dfs.printPath(5);
+    System.out.println();
+
+    // print tree
+    dfs.printTree();
 
     java.util.List<Integer> searchOrders = dfs.getSearchOrder();
     System.out.println(dfs.getNumberOfVerticesFound() + " vertices are searched in this DFS order:");
@@ -27,7 +69,6 @@ class Main {
       if (dfs.getParent(i) != -1)
         System.out.println("parent of " + graph.getVertex(i) +
           " is " + graph.getVertex(dfs.getParent(i)));
-
     
   }
 }
